d2bfafdd410432d5bc33f4118de7eee16c86682e
Al-Abed Jafry <aajafry2@users.noreply.github.com>
2020-08-19 16:22:00 +0600

    Update index.php

--- index.php
+++ index.php
@@ -478,7 +478,7 @@ include("validation.php");
         </div>
     </footer>
     
-    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js" integrity="sha512-WNLxfP/8cVYL9sj8Jnp6et0BkubLP31jhTG9vhL/F5uEZmg5wEzKoXp1kJslzPQWwPT1eyMiSxlKCgzHLOTOTQ==" crossorigin="anonymous"></script>
     <script type='text/javascript' src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script type='text/javascript' src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script type='text/javascript' src="js/main.js"></script>
